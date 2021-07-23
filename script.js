4491064fe5e80221fd2d39b9f6e5ba0bd51e1ef5
Rizky Firman Syah <74675011+Rizfirsy@users.noreply.github.com>
2021-07-23 14:02:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-'use strict';
+
+const hamburger = document.getElementById('menu-icon');
+
+
+hamburger.addEventListener('click', function () {
+	document.getElementById('menu-container').style.display = 'block';
+});
